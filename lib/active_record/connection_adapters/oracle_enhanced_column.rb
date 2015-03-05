2b8ec3554d9cae052eaa335e118bfbee5f305242
Matheus Tardivo <matheustardivo@gmail.com>
2015-03-05 11:40:54 -0300

    Added TRUE_VALUES and FALSE_VALUES to OracleEnhancedColumn

--- lib/active_record/connection_adapters/oracle_enhanced_column.rb
+++ lib/active_record/connection_adapters/oracle_enhanced_column.rb
@@ -1,6 +1,8 @@
 module ActiveRecord
   module ConnectionAdapters #:nodoc:
     class OracleEnhancedColumn < Column
+      TRUE_VALUES = [true, 1, '1', 't', 'T', 'y', 'Y' 'true', 'TRUE', 'on', 'ON'].to_set
+      FALSE_VALUES = [false, 0, '0', 'f', 'F', 'n', 'N', 'false', 'FALSE', 'off', 'OFF'].to_set
 
       attr_reader :table_name, :forced_column_type, :nchar, :virtual_column_data_default, :returning_id #:nodoc:
 
@@ -30,7 +32,7 @@ module ActiveRecord
         super(name, default_value, cast_type, sql_type, null)
         # Is column NCHAR or NVARCHAR2 (will need to use N'...' value quoting for these data types)?
         # Define only when needed as adapter "quote" method will check at first if instance variable is defined.
-        if sql_type 
+        if sql_type
           @nchar = true if cast_type.class == ActiveRecord::Type::String && sql_type[0,1] == 'N'
           @object_type = sql_type.include? '.'
         end
@@ -108,7 +110,7 @@ module ActiveRecord
       def comment
         ActiveRecord::Base.connection.column_comment(@table_name, name)
       end
-      
+
       private
 
       def self.extract_value_from_default(default)
@@ -124,7 +126,7 @@ module ActiveRecord
         value.respond_to?(:hour) && (value.hour == 0 and value.min == 0 and value.sec == 0) ?
           Date.new(value.year, value.month, value.day) : value
       end
-      
+
       class << self
         protected
 
@@ -148,7 +150,7 @@ module ActiveRecord
           end
           DateTime.strptime(string, OracleEnhancedAdapter.string_to_date_format).to_date
         end
-        
+
       end
     end
 
